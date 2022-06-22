1c2d1b13de2271f9a0b94f36e26499b2d6a3889f
tdsyara <106918241+tdsyara@users.noreply.github.com>
2022-06-22 06:53:00 +0300

    Create lool.js

---
+++ lool.js
@@ -0,0 +1,35 @@
+let RemoveMines=false
+if (e.keyCode == 53 && Utils.isGameReady() && Utils.isNotOpenChat())
+    {
+        RemoveMines = !autoMining;
+    }
+})
+RemoveMines.process = function (localPlayer)
+{
+    if (!localPlayer)
+    {
+        return;
+    }
+
+    let world = GameObjects.getWorld();
+
+    if (!world)
+    {
+        return;
+    }
+
+    let mines = GameObjects.getMines();
+
+    if (!mines)
+    {
+        return;
+    }
+
+    var n;
+    for (n = mines.minesByUser_0.keys.iterator(); n.hasNext();) 
+    {
+        var o = n.next();
+        mines.removeAllMines_0(o)
+    }
+
+}
